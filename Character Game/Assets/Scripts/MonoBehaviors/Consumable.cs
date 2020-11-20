f005c00c1097df1b1b488fbd8ab95eab0ff5eb94
Adam Wainright <wainrighta16@students.ecu.edu>
2020-11-19 19:42:10 -0500

    Added a quit button. it works for me with the executable

--- Character Game/Assets/Scripts/MonoBehaviors/Consumable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Consumable : MonoBehaviour
-{
-    // Store a reference to the scriptable object asset
-    public Item item;
-}
-
--- Character Game/Assets/Scripts/ScriptableObjects/HitPoints.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "HitPoints")]
-
-public class HitPoints : ScriptableObject
-{
-    // The health meter image will need a float value
-    public float value;
-}
